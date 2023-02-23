614763e2831cb5f1bdb0f046192184cca6ee37c1
Srikanth Bhupathi <srikanthbhupathi001@gmail.com>
2023-02-23 12:32:33 -0600

    initi

---
+++ Hello.java
@@ -0,0 +1,19 @@
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        int i =1;
+       while(i<=5){
+System.out.println( "Hi "  + i );
+int j=1;
+while(j<=3)
+{
+    System.out.println("Hello" + j);
+    j++;
+}
+       i++;
+    
+}
+    }
+}
+
