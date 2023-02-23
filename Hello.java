5056028623a461951385bfb149686df0d4ccdfd1
Srikanth Bhupathi <srikanthbhupathi001@gmail.com>
2023-02-23 16:27:41 -0600

    created two methods java program

--- Hello.java
+++ Hello.java
@@ -1,19 +1,24 @@
+class Computer{
+
+  public void playmusic(){
+System.out.println("Play Some Random Music");
+    }
+
+    public String getMePen(int cost){
+        if (cost >= 10)
+       return "pen";
+       else{
+        return "Nothing";
+       }
+    }
+}
 public class Hello
 {
     public static void main(String[] args)
     {
-        int i =1;
-       while(i<=5){
-System.out.println( "Hi "  + i );
-int j=1;
-// while(j<=3)
-// {
-//     System.out.println("Hello mama" + j);
-//     j++;
-// }
-       i++;
-    
-}
+    Computer obj = new Computer();
+    obj.playmusic();
+   String Str = obj.getMePen(5);
+   System.out.println(Str);
     }
 }
-
